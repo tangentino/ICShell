281414bcd1c3800c31ed133ac55c84558296c545
Tangentino <tankongpithaksilp@gmail.com>
2021-05-09 22:51:02 +0700

    Implemented basic shell with execvp

--- README.md
+++ README.md
@@ -8,4 +8,8 @@ https://www.geeksforgeeks.org/making-linux-shell-c/
 
 https://stackoverflow.com/questions/43681859/implementing-a-command-line-interpreter-in-c-special-case
 
+https://brennan.io/2015/01/16/write-a-shell-in-c/
+
 https://github.com/szholdiyarov/command-line-interpreter/blob/master/myshell.c
+
+https://github.com/brenns10/lsh/blob/407938170e8b40d231781576e05282a41634848c/src/main.c
--- icsh.c
+++ icsh.c
@@ -1,5 +1,163 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <string.h> 
+#include <stdlib.h>
+
+#define TOK_DELIM " \t\r\n"
+#define RED "\033[0;31m"
+#define RESET "\e[0m"
+
+char *read_line();
+char **split_line(char *);
+int exit_shell(char **);
+int execute(char **);
+
+/*-------------------*
+ * BUILT IN COMMANDS *
+ *-------------------*/
+
+int exit_shell(char **args) {
+	// Exit command
+	printf("Goodbye! :( \n");
+	return 0;
+}
+
+/*-----------------*
+ * COMMAND PARSING *
+ *-----------------*/
+
+char * read_line() {
+
+	// Function to read user commands that are entered into the shell
+	
+	int buffsize = 1024;
+	int position = 0;
+	char * pointer = malloc(sizeof(char) * buffsize); // Char pointer
+	int c;
+	
+	if (!pointer) {
+		// Check if allocation was done properly
+		fprintf(stderr, "%sAllocation error%s\n", RED, RESET);
+		exit(EXIT_FAILURE);
+	}
+	
+	while (1) {
+		// Loop through entire command and get every char
+		c = getchar();
+		if (c == EOF || c == '\n') {
+			// If EOF or \n then end
+			pointer[position] = '\0';
+			return pointer;
+		} else {
+			// Else store the char in pointer
+			pointer[position] = c;
+		}
+		position++;
+		
+		if (position >= buffsize) {
+			// If size of pointer is bigger than the initial size then realloc
+			buffsize += 1024;
+			pointer = realloc(pointer, buffsize);
+			
+			if (!pointer) {
+				// Check if allocation was done properly
+				fprintf(stderr, "%sAllocation error%s\n", RED, RESET);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
+
+char * * split_line(char * line) {
+
+	// Function for splitting the line using strtok() to get command and args
+	
+	int buffsize = 1024, position = 0;
+	char * * tokens = malloc(buffsize * sizeof(char *));
+	char * token;
+	
+	if (!tokens) {
+		// Allocation check
+		fprintf(stderr, "%sAllocation error%s\n", RED, RESET);
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(line, TOK_DELIM);
+	while (token != NULL) {
+		tokens[position] = token;
+		position++;
+		
+		if (position >= buffsize) {
+			buffsize += 1024;
+			tokens = realloc(tokens, buffsize * sizeof(char * ));
+			
+			if (!tokens) {
+				// Allocation check
+				fprintf(stderr, "%sAllocation error%s\n", RED, RESET);
+				exit(EXIT_FAILURE);
+			}
+		}
+		
+		token = strtok(NULL, TOK_DELIM);
+	}
+	
+	tokens[position] = NULL;
+	
+	return tokens;
+	
+}
+
+/*-----------------*
+ * SHELL EXECUTION *
+ *-----------------*/
+
+int execute(char * * args) {
+
+	// Execute command using execvp and fork
+	
+	pid_t cpid;
+	int status;
+	
+	if (strcmp(args[0], "exit") == 0) {
+		return exit_shell(args);
+	}
+	
+	cpid = fork();
+	
+	if (cpid == 0) {
+		if (execvp(args[0],args) < 0)
+			printf("icsh: bad command \n");
+		exit(EXIT_FAILURE);
+	}
+	
+	else if (cpid < 0)
+		printf(RED "Error forking" RESET "\n");
+	else {
+		waitpid(cpid, & status, WUNTRACED);
+	}
+	
+	return 1;
+}
+
+void loop() {
+
+	// Function for the main shell loop
+
+	char * line;
+	char * * args;
+	int running = 1;
+
+	do {
+		printf("icsh $: "); // Print command prompt symbol
+		line = read_line();
+		args = split_line(line);
+		running = execute(args);
+		free(line);
+		free(args);
+	} while (running);
+
+}
 
 int main() {
-	printf("Hello World!");
+	printf("Starting IC shell... \n");
+	loop();
+	return 0;
 }
